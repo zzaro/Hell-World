8a5d0b4998fcbcc02f21868e3079e0c57e2aa2f9
MoNireu <monireu.dev@gmail.com>
2020-06-12 16:22:01 +0900

    feat: 회원가입 UI 추가.
    회원가입 UI (activity_sign_up.xml) 추가.

--- app/src/main/java/com/example/porte/SignIn.kt
+++ app/src/main/java/com/example/porte/SignIn.kt
@@ -2,11 +2,19 @@ package com.example.porte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignIn : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        signIn_btn.setOnClickListener {
+        }
+
+        signUp_btn.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/porte/SignUp.kt
@@ -0,0 +1,12 @@
+package com.example.porte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
