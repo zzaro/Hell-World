e61ef4254d338395f065da1d3996a0557190b248
MoNireu <monireu.dev@gmail.com>
2020-06-13 15:23:45 +0900

    fix: minSdkVersion 16에서 21로 변경.

--- app/src/main/java/com/example/porte/ui/parkingLotInfo/ParkingLotInfoFragment.kt
+++ app/src/main/java/com/example/porte/ui/parkingLotInfo/ParkingLotInfoFragment.kt
@@ -1,11 +1,14 @@
 package com.example.porte.ui.parkingLotInfo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.creageek.segmentedbutton.SegmentedButton
 import com.example.porte.R
+import kotlinx.android.synthetic.main.fragment_parking_lot_info.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,8 +20,36 @@ class ParkingLotInfoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val root = inflater.inflate(R.layout.fragment_parking_lot_info, container, false)
+        val segmented: SegmentedButton = root.findViewById(R.id.segment_control)
+        segmented {
+            // set initial checked segment (null by default)
+            initialCheckedIndex = 0
+
+            // init with segments programmatically without RadioButton as a child in xml
+            initWithItems {
+                // takes only list of strings
+                listOf("Today", "This week", "This month")
+            }
+
+            // notifies when segment was checked
+            onSegmentChecked { segment ->
+                Log.d("creageek:segmented", "Segment ${segment.text} checked")
+            }
+            // notifies when segment was unchecked
+            onSegmentUnchecked { segment ->
+                Log.d("creageek:segmented", "Segment ${segment.text} unchecked")
+            }
+            // notifies when segment was rechecked
+            onSegmentRechecked { segment ->
+                Log.d("creageek:segmented", "Segment ${segment.text} rechecked")
+            }
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parking_lot_info, container, false)
+        return root
+
+
     }
 
 }
