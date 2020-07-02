3570b8174130ff95123a8642af6bd33f8f068c5a
yyyoonho <yoonho997@gmail.com>
2020-07-02 17:23:51 +0900

    no message

--- app/src/main/java/com/example/porte/ui/etc/EtcData.kt
+++ app/src/main/java/com/example/porte/ui/etc/EtcData.kt
@@ -15,7 +15,8 @@ data class EtcBanner(
 data class EtcItem(
     override val imageId: Int,
     val itemName: String,
-    val itemInfo: String
+    val itemInfo: String,
+    val link: String
 ) : EtcContent
 
 /* RecyclerView의 item에 사용할 enum class 선언
@@ -24,9 +25,9 @@ data class EtcItem(
  */
 enum class EtcData(val etcType: Int, val etcContent: EtcContent) {
     ETC_ITEM1(EtcRecyclerAdapter.BANNER, EtcBanner(R.drawable.banner1)),
-    ETC_ITEM2(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.amenity, "편의시설", "편안함과 쾌적함을 제공합니다")),
-    ETC_ITEM3(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.duty_free, "온라인 면세점", " 한ㆍ중ㆍ일ㆍ영 인터넷면세점을 통해 최고의 상품과 서비스를 제공합니다")),
+    ETC_ITEM2(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.amenity, "편의시설", "편안함과 쾌적함을 제공합니다.","https://www.airport.kr/ap/ko/svc/getFacilityMain.do")),
+    ETC_ITEM3(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.duty_free, "온라인 면세점", "최고의 상품과 서비스를 제공합니다.","https://kor.lottedfs.com/kr")),
     ETC_ITEM4(EtcRecyclerAdapter.BANNER, EtcBanner(R.drawable.banner2)),
-    ETC_ITEM5(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.restaurant, "식당안내", "인천공항에서의 즐거운 경험을 위한 다양한 식음료 매장을 안내합니다.")),
-    ETC_ITEM6(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.festival, "공항 이벤트", " 공항에서 즐기는 다양한 프로모션!"));
+    ETC_ITEM5(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.restaurant, "식당안내", "다양한 식음료 매장을 안내합니다.","https://www.airport.kr/ap/ko/shp/getFoodInfoMain.do")),
+    ETC_ITEM6(EtcRecyclerAdapter.ITEM, EtcItem(R.drawable.festival, "공항 이벤트", " 공항에서 즐기는 다양한 프로모션!","https://www.airport.kr/ap/ko/shp/getEventLayerPopup.do"));
 }
