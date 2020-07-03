32930051720686af1351f7d11d478e8010822ab9
MoNireu <monireu.dev@gmail.com>
2020-07-04 06:05:12 +0900

    fix: 뷰 스택 수정.

--- app/src/main/java/com/example/porte/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/porte/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.porte.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,13 +9,17 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.porte.MainActivity
 import com.example.porte.R
 import com.example.porte.Shared.UserInfoDatabase
 import com.example.porte.Shared.UserInfoEntity
 import com.example.porte.Util.ImageTransferUtil
+import com.example.porte.ui.signInUp.Profile
+import com.example.porte.ui.signInUp.SignIn
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.CoroutineScope
@@ -33,22 +38,36 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
 
-        root.findViewById<ImageView>(R.id.home_logout).setOnClickListener {
+        val logoutBtn = root.findViewById<ImageView>(R.id.home_logout)
+        val profileImageiew = root.findViewById<ImageView>(R.id.home_profile)
+
+
+
+        logoutBtn.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 dao.deleteAllUserInfo()
             }
             FirebaseAuth.getInstance().signOut()
+            Toast.makeText(requireContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(requireContext(), SignIn::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
 
 
+        profileImageiew.setOnClickListener{
+            val intent = Intent(requireContext(), Profile::class.java)
+            startActivity(intent)
+        }
 
         CoroutineScope(Dispatchers.IO).launch {
             Log.d("log", dao.selectUserInfo(FirebaseAuth.getInstance().currentUser?.email!!).userName)
             val stringImg = dao.selectUserInfo(FirebaseAuth.getInstance().currentUser?.email!!).userImg
-            val profileImageiew = root.findViewById<ImageView>(R.id.home_profile)
             profileImageiew.setImageBitmap(ImageTransferUtil.changeStirngToBitmap(stringImg!!))
             profileImageiew.clipToOutline = true
         }
